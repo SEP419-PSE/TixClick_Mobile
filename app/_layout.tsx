b3f38e883ca4eb19f3787ef7c05c3329afc36387
Huy-techSw <anchorle3543@gmail.com>
2025-04-09 17:57:55 +0700

    navigate to home and ticket

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,11 +1,14 @@
+import { NavigationContainer } from "@react-navigation/native";
 import { AuthProvider } from "./context/AuthContext";
 import "./global.css";
-import LoginScreen from "./screens/Auth/LoginScreen";
+import AppNavigator from "./navigation/AppNavigator";
+import { StatusBar } from "react-native";
 
 export default function RootLayout() {
   return (
     <AuthProvider>
-      <LoginScreen />
-    </AuthProvider>
+    <StatusBar barStyle="light-content" backgroundColor="#121212" />
+    <AppNavigator />
+  </AuthProvider>
   );
 }
